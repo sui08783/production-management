c97a02d3a1fb72beea8c0f38b8108120f8ccf778
sui <davkii8@gmail.com>
2026-02-18 00:14:40 +0900

    initial clean commit

---
+++ src/main/java/com/sui/production_management/service/TaskEntity.java
@@ -0,0 +1,43 @@
+package com.sui.production_management.service;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "product_name", nullable = false)
+  private String productName;
+
+  @Column(name = "machine_name", nullable = false)
+  private String machineName;
+
+  @Column(name = "deadline", nullable = false)
+  private LocalDate deadline;
+
+  @Column(name = "quantity", nullable = false)
+  private int quantity;
+
+  @Column(name = "create_day", nullable = false)
+  private LocalDateTime createDay;
+
+
+
+}
